100a0f1d6161395a058b8ef1f66787e47862c1f2
Gonzalo Valenzuela <gvalenzuela@TC.cl>
2025-07-15 11:36:22 -0400

    Corrijo importación del componente principal

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GonzaTracker from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GonzaTracker from './GonzaTracker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GonzaTracker />
   </React.StrictMode>
-)
+);
